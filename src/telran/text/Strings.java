577495ffb06e9be2619def91ee83355d6ab6a7d0
LudaTverday <Ludachka22@gmail.com>
2023-06-27 12:23:42 +0300

    initial code and "regex" for IP

---
+++ src/telran/text/Strings.java
@@ -0,0 +1,25 @@
+package telran.text;
+
+public class Strings {
+	public static String javaVariableName() {
+		
+		return "[a-zA-Z$][\\w$]*|_[\\w$]+";
+	}
+	
+	public static String zero_300 () {
+		
+		return "[3-9]\\d?|[1-2]\\d\\d?|300|0";
+	}
+	
+	public static String ipV4Octet () {
+		//positive number from zero to 255;
+		// and leading zeros are allowed;
+		return "[0-1]?\\d{1,2}|2[0-4]\\d|25[0-5]";
+	}
+	public static String ipV4 () {
+		String number = ipV4Octet();
+		//four ipV4 octets separated by dot 123.123.255.01
+		return "(("+ipV4Octet()+")\\.){3}("+ipV4Octet()+")";
+	}
+
+}
