9e8be4083ea412d5ac5747a8e355fd3139c65a4a
LudaTverday <Ludachka22@gmail.com>
2023-06-28 18:58:38 +0300

    chenging method ipV4

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -14,11 +14,17 @@ public class Strings {
 	public static String ipV4Octet () {
 		//positive number from zero to 255;
 		// and leading zeros are allowed;
-		return "[0-1]?\\d{1,2}|2[0-4]\\d|25[0-5]";
+		//return "[0-1]?\\d{1,2}|2[0-4]\\d|25[0-5]";
+		return "([01]?\\d\\d?|2([0-4]\\d|5[0-5]))";
 	}
 	public static String ipV4 () {
 		//four ipV4 octets separated by dot 123.123.255.01
-		return "(("+ipV4Octet()+")\\.){3}("+ipV4Octet()+")";
+		//return "(("+ipV4Octet()+")\\.){3}("+ipV4Octet()+")";
+		//return String.format("(%s[.]){3}%s", ipV4Octet(),ipV4Octet());
+		return String.format("(%1$s\\.){3}%1$s", ipV4Octet());
+	}
+	public static String arithmeticExpression() {
+		
 	}
 
 }
